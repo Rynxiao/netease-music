61d99c08ddaabb19dca4dfef03fc6c5ab7f202c0
rynxiao <yuzhongzi91@sina.com>
2017-11-07 12:50:57 +0800

    add shot

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,15 @@ var listPosts = [
 /* GET home page. */
 router.get('/', function(req, res, next) {
     fs.readdir(media, function(err, names) {
+
+        console.log(names);
         var first = names[0];
+
+        if (first.indexOf('mp3') === -1) {
+            first = names[1];
+            names = names.slice(1);
+        }
+
         var song = first.split(' - ')[1].replace('.mp3', '');
         var singer = first.split(' - ')[0];
 
